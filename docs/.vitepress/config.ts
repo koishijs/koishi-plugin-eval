cf2d4cdffc075acdf672f11c3da8e797f10ebc89
Shigma <shigma10826@gmail.com>
2022-11-08 15:14:49 +0800

    docs: setup new docs

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vitepress'
+import { defineConfig } from '@koishijs/vitepress'
 
 export default defineConfig({
   lang: 'zh-CN',
@@ -7,6 +7,7 @@ export default defineConfig({
 
   themeConfig: {
     outline: [2, 3],
+
     sidebar: [{
       text: '指南',
       items: [
@@ -21,7 +22,21 @@ export default defineConfig({
         { text: '主线程 API', link: '/worker.md' },
         { text: '沙箱 API', link: '/sandbox.md' },
       ],
+    }, {
+      text: '更多',
+      items: [
+        { text: 'Koishi 官网', link: 'https://koishi.chat' },
+        { text: '支持作者', link: 'https://afdian.net/a/shigma' },
+      ],
     }],
+
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/koishijs/koishi-plugin-eval' }
+    ],
+
+    editLink: {
+      pattern: 'https://github.com/koishijs/koishi-plugin-eval/edit/master/docs/:path',
+    },
   },
 
   vite: {
